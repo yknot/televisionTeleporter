3a1a9a78efbf0d74308678e85397729688e2a658
VBajaj <verrrn@gmail.com>
2014-03-25 21:13:17 -0400

    Updated to grab ID number as first element in line of output.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ televisionTeleporter
 ====================
 
 A continously running media management script
+
+Update 3/25/2014:
+Made changes to split by line, then split each line by whitespace. Now gets ID number and torrent name.
--- moveEpisodes.py
+++ moveEpisodes.py
@@ -10,34 +10,45 @@ process = subprocess.Popen(transmission_list.split(), stdout=subprocess.PIPE)
 transmission_output = process.communicate()[0]
 
 # split output into strings
-transmission_list = transmission_output.split()
+transmission_lines = transmission_output.splitlines()
+nlines=len(transmission_lines)
 
+# for each line
+for i in transmission_lines:
 
-# if one or more of the torrents are seeding
-if 'Seeding' in transmission_list:
+        transmission_list=transmission_lines[i].split()
+        # if one or more of the torrents are seeding
+        if 'Seeding' in transmission_list:
 
-	# store index of the seeding
-	index = transmission_list.index('Seeding')
-	ids = []
+                # store index of the seeding
+                #Commenting out older code# index = transmission_list.index('Seeding')
+                #ids = []
 
-	# loop through and get id indexes 
-	for x in transmission_list:
+                # get torrent ID number
+        `       tor_id= transmission_list[0]
 
-		# if only numbers without . (trying to find ints)
-		if re.match("^[0-9]*$", x):
-			ids.append(transmission_list.index(x))
+                # get torrent name (for moving torrents)
+                tor_name=transmission_list[len(transmission_list)-1]
+                # remove torrent command with id
+                remove_torrent_cmd = "transmission-remote -t " + str(tor_id) + " -r"
+                process = subprocess.Popen(remove_torrent_cmd.split(), stdout=subprocess.PIPE)
+                remove_torrent_output = process.communicate()[0]
 
-	# for each id
-	for y in ids:
+                # make sure success
 
-		# if the index of seeding is the appropriate distance away such that it is an id
-		if index - y > 4 and index - y < 10:
+                # Old code comments, to keep in case I broke something
+                # loop through and get id indexes 
+                # for x in transmission_list:
+                
+                        # if only numbers without . (trying to find ints)
+                #        if re.match("^[0-9]*$", x):
+                #                ids.append(transmission_list.index(x))
 
-			# remove torrent command with id
-			remove_torrent_cmd = "transmission-remote -t " + str(transmission_list[y]) + " -r"
-			process = subprocess.Popen(remove_torrent_cmd.split(), stdout=subprocess.PIPE)
-			remove_torrent_output = process.communicate()[0]
+                # for each id
+                # for y in ids:
 
-			# make sure success
+                #        # if the index of seeding is the appropriate distance away such that it is an id
+                #        if index - y > 4 and index - y < 10:
 
+                                
 
